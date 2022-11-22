f8dfae54dc9a464ff84ff5790b3900db28ae735e
abdulqadir742 <abqadirkhan742@gmail.com>
2022-11-22 14:52:01 +0500

    endless recycle workdone

--- app/src/main/java/com/example/implement_api/MainActivity.kt
+++ app/src/main/java/com/example/implement_api/MainActivity.kt
@@ -6,6 +6,9 @@ import android.util.Log
 import androidx.appcompat.app.ActionBar
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import androidx.recyclerview.widget.RecyclerView.OnScrollListener
 import com.example.implement_api.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Response
@@ -15,16 +18,39 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     lateinit var adapter: NewsAdapter
+    private var articles = mutableListOf<Article>()
+    var pageNum = 1
+    var totalresult = -1
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-
+        adapter = NewsAdapter(this@MainActivity,articles)
+        binding.newsList.adapter = adapter
+        val layoutManager1 = LinearLayoutManager(this@MainActivity)
+        binding.newsList.layoutManager = layoutManager1
         supportActionBar?.hide()
+
+       binding.newsList.addOnScrollListener(object : OnScrollListener(){
+           override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+               if (totalresult > layoutManager1.itemCount && layoutManager1.findFirstVisibleItemPosition()>= layoutManager1.itemCount -7)
+               {
+                   pageNum++
+                   getNews()
+               }
+               super.onScrolled(recyclerView, dx, dy)
+           }
+       })
+
+
+
         getNews()
     }
 
     private fun getNews() {
-        val news = NewService.newsInstance.getHeadlines("us",2)
+        val news = NewService.newsInstance.getHeadlines("in",pageNum)
         news.enqueue(object :retrofit2.Callback<News>{
 
             override fun onResponse(call: Call<News>, response: Response<News>) {
@@ -32,9 +58,9 @@ class MainActivity : AppCompatActivity() {
                 if (news!= null)
                 {
                     Log.d("MY News",news.toString())
-                    adapter = NewsAdapter(this@MainActivity,news.articles)
-                    binding.newsList.adapter = adapter
-                    binding.newsList.layoutManager = LinearLayoutManager(this@MainActivity)
+                    totalresult = news.totalResults
+                    articles.addAll(news.articles)
+                    adapter.notifyDataSetChanged()
 
                 }
             }
